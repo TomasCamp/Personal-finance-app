dd1f216273acb541e0d74b3a90af20bbb4ada859
Tomás Campanella <tomyecamp@gmail.com>
2025-04-06 21:04:05 -0300

    Modifico DEBUG de settings

--- gestor_de_gastos/settings.py
+++ gestor_de_gastos/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e7pj82$fel$()n-o-qy&935*$tya5w**zo%tu0&v@$z$d)i%31'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
